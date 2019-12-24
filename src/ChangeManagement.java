31e152bedb3e1a0ac47e817865eda85273e146d3
maxwell94 <maxirsula@gmail.com>
2019-12-24 10:01:35 +0100

    inizio

---
+++ src/ChangeManagement.java
@@ -0,0 +1,11 @@
+/**
+ * Classe che gestisce tutto il Change Management. 
+ * Ciascun metodo ha un ruolo ben preciso , per esempio:
+ * il metodo "appoggioChangedGames()" andrà nel tab "Appoggio Changed Games" 
+ * e gestirà tutto il lavoro che dovrà fare in "Appoggio Changed Games",
+ * il metodo "checkSum()" andrà nel tab "Checksum" e gestirà tutto il lavoro che dovrà
+ * fare in checksum e così via. */
+
+public class ChangeManagement {
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,21 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println("Ciao");
 	}
-
+	
 }
